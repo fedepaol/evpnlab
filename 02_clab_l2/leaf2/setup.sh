ae4256d4f0d9b1f897952c44d03da77bf18f4414
Federico Paolinelli <fpaoline@redhat.com>
2024-05-17 16:40:40 +0200

    L2 only: clean up the config of the leaves
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- 02_clab_l2/leaf2/setup.sh
+++ 02_clab_l2/leaf2/setup.sh
@@ -11,18 +11,13 @@ ip link set red up
 ip link add br10 type bridge
 ip link set br10 master red
 ip link set br10 addr aa:bb:cc:00:00:64
-ip addr add 192.168.10.1/24 dev br10
 
 ip link add vni110 type vxlan local 100.65.0.2 dstport 4789 id 110 nolearning
 ip link set vni110 master br10 addrgenmode none
-
-
 ip link set vni110 type bridge_slave neigh_suppress on learning off
 
 ip link set vni110 up
 ip link set br10 up
 
-ip addr add 192.168.10.9/24 dev eth2
-ip addr add 192.168.10.10/24 dev eth3
 ip link set eth2 master br10
 ip link set eth3 master br10
